6a69525548d8be02c14feb9eec7c8f1ce06bcbc2
hoyuet <924892426@qq.com>
2017-05-12 18:27:42 +0800

    add

---
+++ 积分商城页面/lib/preLoad.js
@@ -0,0 +1,79 @@
+$.MyCommon = {
+    PageLoading: function (options) {
+        var defaults = {
+            opacity: 1,
+            //loading页面透明度
+            backgroundColor: "#fff",
+            //loading页面背景色
+            borderColor: "#bbb",
+            //提示边框颜色
+            borderWidth: 1,
+            //提示边框宽度
+            borderStyle: "solid",
+            //提示边框样式
+            loadingTips: "Loading, please wait...",
+            //提示文本
+            TipsColor: "#666",
+            //提示颜色
+            delayTime: 500,
+            //页面加载完成后，加载页面渐出速度
+            zindex: 999,
+            //loading页面层次
+            sleep: 0
+            //设置挂起,等于0时则无需挂起
+
+        }
+        var options = $.extend(defaults, options);
+
+        //获取页面宽高
+        var _PageHeight = document.documentElement.clientHeight,
+        _PageWidth = document.documentElement.clientWidth;
+console.log(_PageHeight,_PageWidth)
+        //在页面未加载完毕之前显示的loading Html自定义内容
+//      var _LoadingHtml = '<div id="loadingPage" style="position:fixed;left:0;top:0;_position: absolute;width:100%;height:' + _PageHeight + 'px;background:' + options.backgroundColor + ';opacity:' + options.opacity + ';filter:alpha(opacity=' + options.opacity * 100 + ');z-index:' + options.zindex + ';"><div id="loadingTips" style="position: absolute; cursor1: wait; width: auto;border-color:' + options.borderColor + ';border-style:' + options.borderStyle + ';border-width:' + options.borderWidth + 'px; height:80px; line-height:80px; padding-left:80px; padding-right: 5px;border-radius:10px;  background: ' + options.backgroundColor + ' url(/积分商城页面/image/loading.gif) no-repeat 5px center; color:' + options.TipsColor + ';font-size:20px;">' /*+ options.loadingTips*/ + '</div></div>';
+var _LoadingHtml = '<div id="loadingPage" style="position:fixed;left:0;top:0;_position: absolute;width:'+_PageWidth+'px;height:' + _PageHeight + 'px;background:' + options.backgroundColor + ';opacity:' + options.opacity + ';filter:alpha(opacity=' + options.opacity * 100 + ');z-index:' + options.zindex + ';"><div id="loadingTips" style="position: absolute; cursor1: wait; width: 80px;' +  'height:80px; line-height:80px; background: ' + options.backgroundColor + ' url(/积分商城页面/image/loading.gif) no-repeat center center; color:' + options.TipsColor + ';font-size:20px;">' /*+ options.loadingTips*/ + '</div></div>';
+        //呈现loading效果
+        $("body").append(_LoadingHtml);
+
+        //获取loading提示框宽高
+        var _LoadingTipsH = document.getElementById("loadingTips").clientHeight,
+        _LoadingTipsW = document.getElementById("loadingTips").clientWidth;
+
+        //计算距离，让loading提示框保持在屏幕上下左右居中
+        var _LoadingTop = _PageHeight > _LoadingTipsH ? (_PageHeight - _LoadingTipsH) / 2 : 0,
+        _LoadingLeft = _PageWidth > _LoadingTipsW ? (_PageWidth - _LoadingTipsW) / 2 : 0;
+
+        $("#loadingTips").css({
+            "left": _LoadingLeft + "px",
+            "top": _LoadingTop + "px"
+        });
+
+        //监听页面加载状态
+        document.onreadystatechange = PageLoaded;
+
+        //当页面加载完成后执行
+        function PageLoaded() {
+            if (document.readyState == "complete") {
+                var loadingMask = $('#loadingPage');
+
+                setTimeout(function () {
+                    loadingMask.animate({
+                        "opacity": 0
+//"opacity": 1
+                    },
+                    options.delayTime,
+                    function () {
+                        $(this).hide();
+$(this).show();
+
+                    });
+
+                },
+                options.sleep);
+
+            }
+        }
+    }
+}
+
+ 	$.MyCommon.PageLoading({ sleep: 500 });
