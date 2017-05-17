d43b3a3a9b463df5c5266151428c1b1c9f0822ec
hoyuet <924892426@qq.com>
2017-05-17 17:22:11 +0800

    areapiker

--- 积分商城页面/lib/areaPicker.js
+++ 积分商城页面/lib/areaPicker.js
@@ -592,7 +592,14 @@ $(function() {
 	$("#expressArea").click(function() {
 		$("#areaMask").fadeIn();
 //		$("#areaLayer").animate({"bottom": 0});
-		$('#areaWrap').animate({top:'1.18rem'});
+		$('#areaWrap').animate({top:'1.18rem'},500,function(){
+			 $(this).find('header').css({
+			 	position:'fixed',
+			 	left:'0',
+			 	top:'3.34rem',
+			 	width:'100%'
+			 });
+		});
 	});
 	/*关闭省市区选项*/
 	$("#areaMask, #closeArea").click(function() {
